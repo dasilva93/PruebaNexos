8113d74baceb5c251512d94e8f8ad9b4850cc152
David Silva <dasilva6793@gmail.com>
2022-09-15 19:19:41 -0500

    Commit
    Cambio en war

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # PruebaNexos
+
+Prueba técnica desarrollador Java para la compañia nexos software realizada por el ingeniero en sistemas David Orlando Silva Amaya.
+
+#Especificaciones
+Java 11
+PrimeFaces 11
+h2 2.1.214
+hibernate 6.1.3
+
+# Proceso de instalacion
+
+1. Entrar en la carpeta realise.
+2. Subir archivo "PruebaNexos.war" en un servidor wildfly 26+.
--- src/main/java/com/pruebanexos/action/UserManager.java
+++ src/main/java/com/pruebanexos/action/UserManager.java
@@ -1,8 +1,6 @@
 package com.pruebanexos.action;
 
 import java.io.Serializable;
-import java.util.Date;
-import java.util.List;
 
 import javax.enterprise.context.SessionScoped;
 import javax.faces.context.ExternalContext;
@@ -11,8 +9,6 @@ import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-
-import com.pruebanexos.entity.Departamento;
 
 @Named
 @SessionScoped
@@ -29,22 +25,7 @@ public class UserManager implements Serializable {
 		externalContext.getSessionMap().put("userManager", this);
 		
 		entityManagerFactory = Persistence.createEntityManagerFactory("Persistence");				
-		entityManager = entityManagerFactory.createEntityManager();
-		System.out.println("=========== " + entityManager);
-		List<Departamento> lst = (List<Departamento>) entityManager.createQuery("FROM Departamento").getResultList();
-		System.out.println("lst.size() " + lst.size());
 		
-		entityManager.getTransaction().begin();
-		entityManager.persist(new Departamento("01","Prueba", new Date()));
-		entityManager.persist(new Departamento("02","Prueba2", new Date()));
-		entityManager.getTransaction().commit();
-		
-		List<Departamento> lst2 = (List<Departamento>) entityManager.createQuery("FROM Departamento").getResultList();
-		System.out.println("lst.size() " + lst2.size());
-		for(Departamento d:lst2) {
-			System.out.println(d.toString());
-		}
-		entityManager.close();
 	}	
 
 	public String getTitle() {
